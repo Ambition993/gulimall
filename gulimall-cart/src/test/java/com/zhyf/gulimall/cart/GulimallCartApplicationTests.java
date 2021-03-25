7d56ac256492c94cbbd3b2f66e78b3f456a5d922
Ambition993 <nibainle@gmail.com>
2021-03-25 20:40:52 +0800

    购物车初始化

---
+++ gulimall-cart/src/test/java/com/zhyf/gulimall/cart/GulimallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhyf.gulimall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
