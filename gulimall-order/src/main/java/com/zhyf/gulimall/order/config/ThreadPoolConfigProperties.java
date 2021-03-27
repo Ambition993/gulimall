d01e68aa2141cd471697dcff4f7acc3bf158a1d0
Ambition993 <nibainle@gmail.com>
2021-03-27 19:59:29 +0800

    order 服务页面调通

---
+++ gulimall-order/src/main/java/com/zhyf/gulimall/order/config/ThreadPoolConfigProperties.java
@@ -0,0 +1,14 @@
+package com.zhyf.gulimall.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "gulimall.thread")
+@Data
+public class ThreadPoolConfigProperties {
+    private Integer coreSize;
+    private Integer maxsize;
+    private Integer keepAliveTime;
+}
